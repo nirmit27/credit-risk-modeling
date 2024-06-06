1d6040a566fc629298d35156a78b90dd21a7487a
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-06-06 22:01:56 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -10,9 +10,6 @@ import os
 import pandas as pd
 import numpy as np
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 from scipy.stats import f_oneway as anova
 from scipy.stats import chi2_contingency as chi2
 from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
