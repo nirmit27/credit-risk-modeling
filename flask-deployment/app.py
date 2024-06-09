bd75449a6ea85f5b7c060dd83c18519e298b5840
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-06-09 16:24:01 +0530

    first run

--- flask-deployment/app.py
+++ flask-deployment/app.py
@@ -1,3 +1,4 @@
+import pickle
 import subprocess
 from configparser import ConfigParser as cfgp
 from flask import Flask, render_template, request, url_for
@@ -6,33 +7,43 @@ config = cfgp()
 config.read('settings.cfg')
 config_section = config['DEPLOYMENT']
 
+model = pickle.load(open("model.pkl", "rb"))
 port: int = int(config_section['PORT'])
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET"])
 def index():
-    fields: list[str] = ["ROCE (%)", "CASA (%)", "Return on Equity/Networth (%)",
-                         "Non-interest Income/Total Assets (%)", "Operating Profit/Total Assets (%)", "Operating Expenses/Total Assets (%)", "Interest Expenses/Total Assets (%)", "Face Value"]
+    fields: list[str] = ['ROCE (%)', 'CASA (%)', 'Return on Equity/Networth (%)',
+                         'Non-interest Income/Total Assets (%)', 'Operating Profit/Total Assets (%)', 'Operating Expenses/Total Assets (%)', 'Interest Expenses/Total Assets (%)', 'Face Value']
+
     return render_template("index.html", title="Inputs", subtitle="EPS Prediction using ML", fields=fields)
 
 
 @app.route('/predict', methods=["POST"])
 def predict():
-    n1: str = request.form.get("ip1", "")
-    n2: str = request.form.get("ip2", "")
-    n3: str = request.form.get("ip3", "")
-    n4: str = request.form.get("ip4", "")
-    n5: str = request.form.get("ip5", "")
-    n6: str = request.form.get("ip6", "")
-    n7: str = request.form.get("ip7", "")
-    n8: str = request.form.get("ip8", "")
+    result: str = "Result : "
+
+    v1: float = float(request.form.get('ROCE (%)', "0.00"))
+    v2: float = float(request.form.get('CASA (%)', "0.00"))
+    v3: float = float(request.form.get(
+        'Return on Equity/Networth (%)', "0.00"))
+    v4: float = float(request.form.get(
+        'Non-interest Income/Total Assets (%)', "0.00"))
+    v5: float = float(request.form.get(
+        'Operating Profit/Total Assets (%)', "0.00"))
+    v6: float = float(request.form.get(
+        'Operating Expenses/Total Assets (%)', "0.00"))
+    v7: float = float(request.form.get(
+        'Interest Expenses/Total Assets (%)', "0.00"))
+    v8: float = float(request.form.get('Face Value', "0.00"))
+
     try:
-        test: int = int(n1) + int(n2) + int(n3) + int(n4) + \
-            int(n5) + int(n6) + int(n7) + int(n8)
-        result: str = str(test)
+        pred = model.predict([[v1, v2, v3, v4, v5, v6, v7, v8]])
+        result += str(pred[0])
     except:
-        result: str = "Invalid input"
+        result += "Invalid input!"
+
     return render_template("result.html", result=result, title="Result")
 
 
